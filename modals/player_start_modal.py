d88c5b4db6794d209d7e813a0fd662197968a64c
thearyadev <personal@aryankothari.dev>
2023-06-28 14:37:42 -0400

    player join now mutates the lobby

--- modals/player_start_modal.py
+++ modals/player_start_modal.py
@@ -19,9 +19,5 @@ class PlayerStartModal(nextcord.ui.Modal):
         
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
-        response = f"{interaction.user.mention}'s favourite pet's name is {self.name.value}."
-        if self.description.value != "":
-            response += (
-                f"\nTheir pet can be recognized by this information:\n{self.description.value}"
-            )
+        response = f"{interaction.user.mention}'s favourite pet's name is {self.tank.value}."
         await interaction.send(response)
