b9e02ced6ba249db24740553f5c63af9a1c302e3
thearyadev <personal@aryankothari.dev>
2023-06-28 23:57:41 -0400

    flow changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
-
 import nextcord
 from nextcord.ext import commands
 from modals.player_start_modal import PlayerStartModal
+from modals.create_lobby_modal import CreateLobbyModal
 from dotenv import load_dotenv
 import os
 from views.join_lobby_view import JoinLobbyView
@@ -20,20 +19,49 @@ class Matchmaker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.lobbies = set()
-        
-    @nextcord.slash_command(name="play", description="Join the lobby!", guild_ids=[TESTING_GUILD_ID])
+
+    @nextcord.slash_command(
+        name="play", description="Join the lobby!", guild_ids=[TESTING_GUILD_ID]
+    )
     async def play(self, interaction: nextcord.Interaction):
         # modal = PlayerStartModal()
         # await interaction.response.send_modal(modal)
-        print(self.lobbies)
         await interaction.response.send_message("Joining lobby...")
-    
-    @nextcord.slash_command(name="createlobby", description="Create a new lobby", guild_ids=[TESTING_GUILD_ID])
+
+    @nextcord.slash_command(
+        name="createlobby",
+        description="Create a new lobby",
+        guild_ids=[TESTING_GUILD_ID],
+    )
     async def createlobby(self, interaction: nextcord.Interaction):
-        lobby: Lobby = Lobby(id=uuid4(), players=set())
-        self.lobbies.add(lobby)
-        view = JoinLobbyView(lobby=lobby)
-        await interaction.response.send_message("Lobby created!", view=view)
+        createLobbyModal = CreateLobbyModal()
+        await interaction.response.send_modal(createLobbyModal)
+        await createLobbyModal.wait()
+        if createLobbyModal.game is not None:
+            lobby: Lobby = Lobby(
+                id=uuid4(),
+                players=set(),
+                name=createLobbyModal.lobby_name,
+                guild=interaction.guild,
+                game=createLobbyModal.game,
+                owner=interaction.user,
+            )
+            await lobby.create_channels()
+
+            self.lobbies.add(lobby)
+            lobby.join_message = await lobby.join_text.send(
+                view=JoinLobbyView(lobby=lobby),
+                embed=nextcord.Embed(
+                    title="Welcome to the lobby!",
+                    description="Click the 'Join' button to join the lobby!",
+                    color=nextcord.Color.green(),
+                )
+                .add_field(name="Game", value=lobby.game.name)
+                .add_field(name="Owner", value=lobby.owner.mention)
+                .add_field(name="Lobby ID", value=lobby.id.hex[:4])
+                .add_field(name="Number of Players", value=len(lobby.players)),
+            )
+
 
 bot.add_cog(Matchmaker(bot))
 bot.run(os.getenv("TOKEN"))
