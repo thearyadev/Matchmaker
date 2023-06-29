94604ac76f55b3c787d3651e37e4a748fc79fe56
thearyadev <personal@aryankothari.dev>
2023-06-29 01:17:43 -0400

    fix flow

--- modals/player_start_modal.py
+++ modals/player_start_modal.py
@@ -1,4 +1,6 @@
 import nextcord
+from util.models.rank import OverwatchRank
+from util.models.role import Role
 
 
 class PlayerStartModal(nextcord.ui.Modal):
@@ -7,17 +9,24 @@ class PlayerStartModal(nextcord.ui.Modal):
             "Welcome to the lobby!",
             timeout=5 * 60,  # 5 minutes
         )
+        self.role: Role = None
+        self.rank: OverwatchRank = None
 
-        self.tank = nextcord.ui.TextInput(
-            placeholder="Enter your favourite pet's name...",
-            min_length=3,
-            max_length=25,
-            label="Pet's name",
+        self.role_input = nextcord.ui.TextInput(
+            min_length=4,
+            label="What role would you like to play?",
+            placeholder="TANK, DAMAGE, or SUPPORT",
+            required=True,
         )
-        self.add_item(self.tank)
+        self.rank_input = nextcord.ui.TextInput(
+            min_length=4, label="What is your rank? (Example: DIAMOND 5)", required=True
+        )
+        self.add_item(self.role_input)
+        self.add_item(self.rank_input)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
-        response = (
-            f"{interaction.user.mention}'s favourite pet's name is {self.tank.value}."
+        self.role = Role.__members__.get(self.role_input.value.upper().strip())
+        self.rank = OverwatchRank.__members__.get(
+            "_".join(self.rank_input.value.upper().strip().split(" "))
         )
-        await interaction.send(response)
+        self.stop()
