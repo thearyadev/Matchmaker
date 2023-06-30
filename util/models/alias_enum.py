755ae6775cccfc123490833e5f9a4d0eacfdee6c
thearyadev <personal@aryankothari.dev>
2023-06-30 18:18:22 -0400

    use tuple instead of list to save memory

--- util/models/alias_enum.py
+++ util/models/alias_enum.py
@@ -9,9 +9,9 @@ T = TypeVar("T", bound="AliasEnum")
 
 
 class AliasEnum(Enum):
-    aliases: Optional[list[str]]
+    aliases: Optional[tuple[str]]
 
-    def __new__(cls: Type[T], value: int, aliases: Optional[list[str]] = None):
+    def __new__(cls: Type[T], value: int, aliases: Optional[tuple[str]] = None):
         obj = object.__new__(cls)
         obj._value_ = value
         obj.aliases = aliases
@@ -24,12 +24,10 @@ class AliasEnum(Enum):
         for member in list(cls):
             for member_aliases in (member.name, *member.aliases):
                 if (
-                    (score := fuzz.ratio(
+                    score := fuzz.ratio(
                         standardize(member_aliases), standardize(string)
-                    ))
-                    >= 95
-                ):
+                    )
+                ) >= 95:
                     return member
                 results.append((member, score))
         return max(results, key=lambda value_pair: value_pair[-1])[0]
- 
