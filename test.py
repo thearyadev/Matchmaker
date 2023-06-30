e8631d85b65c9c1a71a8c86db3ff6a5df9de9f99
thearyadev <personal@aryankothari.dev>
2023-06-30 18:17:20 -0400

    nothing

--- test.py
+++ test.py
@@ -8,11 +8,19 @@ import random
 team1 = Team("Team 1")
 team2 = Team("Team 2")
 
-team1.add(Player(user="gnome", role=Role.TANK, total_games=0, rank=OverwatchRank.BRONZE_5))
-team1.add(Player(user="gnome2", role=Role.SUPPORT, total_games=0, rank=OverwatchRank.BRONZE_5))
+team1.add(
+    Player(user="gnome", role=Role.TANK, total_games=0, rank=OverwatchRank.BRONZE_5)
+)
+team1.add(
+    Player(user="gnome2", role=Role.SUPPORT, total_games=0, rank=OverwatchRank.BRONZE_5)
+)
 
-team2.add(Player(user="gnome3", role=Role.SUPPORT, total_games=0, rank=OverwatchRank.BRONZE_5))
-team2.add(Player(user="gnome4", role=Role.DAMAGE, total_games=0, rank=OverwatchRank.BRONZE_5))
+team2.add(
+    Player(user="gnome3", role=Role.SUPPORT, total_games=0, rank=OverwatchRank.BRONZE_5)
+)
+team2.add(
+    Player(user="gnome4", role=Role.DAMAGE, total_games=0, rank=OverwatchRank.BRONZE_5)
+)
 
 shuffle(team1, team2)
 print(team1)
