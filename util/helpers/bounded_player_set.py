1c9325ac6e6d51192678fca237305f9a876b44f6
thearyadev <personal@aryankothari.dev>
2023-07-04 16:13:16 -0400

    Remove role

--- util/helpers/bounded_player_set.py
+++ util/helpers/bounded_player_set.py
@@ -1,26 +1,29 @@
 from dataclasses import dataclass
+from typing import Generic, Set, TypeVar
+
 from util.models.role import Role
-from typing import TypeVar, Generic, Set
 
 T = TypeVar("T")
+
 
 class PlayerDoesntExist(Exception):
     pass
 
+
 class TeamIsFull(Exception):
     pass
 
+
 class BoundedPlayerSet(Generic[T], Set[T]):
-    def __init__(self, size: int, role: Role, *args, **kwargs):
+    def __init__(self, size: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.max_size = size
-        self.role = role
 
     def add(self, player):
         if len(self) < self.max_size:
             super().add(player)
         else:
-            raise TeamIsFull(f"Team role [{self.role}] is full")
+            raise TeamIsFull(f"Team is full")
 
     def remove(self, player):
         if player in self:
