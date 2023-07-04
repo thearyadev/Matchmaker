c4da56efd750264b4fc4187041a2ca9dcd1b4594
thearyadev <personal@aryankothari.dev>
2023-07-04 16:10:31 -0400

    convert team to ABC & add impls for Val and OW

--- tests/test_team.py
+++ tests/test_team.py
@@ -8,51 +8,101 @@ parent_dir = os.path.dirname(current_dir)
 # Add the parent directory to sys.path
 sys.path.append(parent_dir)
 
+import random
+
+from util.helpers.bounded_player_set import BoundedPlayerSet
 from util.models.player import Player
-from util.models.rank import OverwatchRank
+from util.models.rank import OverwatchRank, ValorantRank
 from util.models.role import Role
-from util.models.team import Team, mean_delta, shuffle
+from util.models.team import (
+    OverwatchTeam,
+    ValorantTeam,
+    balance_teams,
+    mean_delta,
+    shuffle,
+)
 
 
-def test_initialization():
-    team = Team(name="team_name")
+def test_overwatch_team_initialization():
+    team = OverwatchTeam(name="team_name")
     assert team.name == "team_name"
-    assert isinstance(team.players, set)
-    assert isinstance(team, set)
+    assert isinstance(team.tank, BoundedPlayerSet)
+    assert isinstance(team.damage, BoundedPlayerSet)
+    assert isinstance(team.support, BoundedPlayerSet)
+    assert team.tank.max_size == 1
+    assert team.damage.max_size == 2
+    assert team.support.max_size == 2
 
 
-def test_add_player():
-    team = Team(name="team_name")
+def test_valorant_team_initialization():
+    team = ValorantTeam(name="team_name")
+    assert team.name == "team_name"
+    assert isinstance(team.players, BoundedPlayerSet)
+    assert team.players.max_size == 5
+
+
+def test_overwatch_add_player():
+    team = OverwatchTeam(name="team_name")
     player = Player(
         user="player", role=Role.TANK, rank=OverwatchRank.GRANDMASTER_1, total_games=12
     )
     team.add(player)
-    assert player in team.players
+    assert player in team
 
 
-def test_remove():
-    team = Team(name="team_name")
+def test_valorant_add_player():
+    team = ValorantTeam(name="team_name")
+    player = Player(
+        user="player", role=None, rank=ValorantRank.ASCENDANT_1, total_games=12
+    )
+    team.add(player)
+    assert player in team
+
+
+def test_overwatch_remove():
+    team = OverwatchTeam(name="team_name")
     player = Player(
         user="player", role=Role.TANK, rank=OverwatchRank.GRANDMASTER_1, total_games=12
     )
     team.add(player)
-    assert player in team.players
+    assert player in team
     team.remove(player)
-    assert player not in team.players
+    assert player not in team
 
 
-def test_iterable():
-    team = Team(name="team_name")
+def test_valorant_remove():
+    team = ValorantTeam(name="team_name")
+    player = Player(
+        user="player", role=None, rank=ValorantRank.ASCENDANT_1, total_games=12
+    )
+    team.add(player)
+    assert player in team
+    team.remove(player)
+    assert player not in team
+
+
+def test_overwatch_iterable():
+    team = OverwatchTeam(name="team_name")
     player = Player(
         user="player", role=Role.TANK, rank=OverwatchRank.GRANDMASTER_1, total_games=12
     )
     team.add(player)
     for player in team:
-        assert player in team.players
+        assert player in team
 
 
-def test_len():
-    team = Team(name="team_name")
+def test_valorant_iterable():
+    team = ValorantTeam(name="team_name")
+    player = Player(
+        user="player", role=None, rank=ValorantRank.ASCENDANT_1, total_games=12
+    )
+    team.add(player)
+    for player in team:
+        assert player in team
+
+
+def test_overwatch_len():
+    team = OverwatchTeam(name="team_name")
     player = Player(
         user="player", role=Role.TANK, rank=OverwatchRank.GRANDMASTER_1, total_games=12
     )
@@ -60,9 +110,18 @@ def test_len():
     assert len(team) == 1
 
 
-def test_mean_delta():
-    team1 = Team(name="team_name")
-    team2 = Team(name="team_name")
+def test_valorant_len():
+    team = ValorantTeam(name="team_name")
+    player = Player(
+        user="player", role=None, rank=ValorantRank.ASCENDANT_1, total_games=12
+    )
+    team.add(player)
+    assert len(team) == 1
+
+
+def test_overwatch_mean_delta():
+    team1 = OverwatchTeam(name="team_name")
+    team2 = OverwatchTeam(name="team_name")
     player1 = Player(
         user="player1", role=Role.TANK, rank=OverwatchRank.GRANDMASTER_1, total_games=12
     )
@@ -102,44 +161,149 @@ def test_mean_delta():
     assert mean_delta(team1, team2) == 11.333333333333332
 
 
-def test_shuffle():
-    team1 = Team(name="team1")
-    team2 = Team(name="team2")
-
-    # Add players to the teams
+def test_valorant_mean_delta():
+    team1 = ValorantTeam(name="team_name")
+    team2 = ValorantTeam(name="team_name")
     player1 = Player(
-        user="player1", role=Role.TANK, rank=OverwatchRank.GRANDMASTER_2, total_games=12
+        user="player1", role=None, rank=ValorantRank.ASCENDANT_1, total_games=12
     )
     player2 = Player(
-        user="player2", role=Role.SUPPORT, rank=OverwatchRank.BRONZE_5, total_games=12
-    )
-    player3 = Player(
-        user="player3", role=Role.DAMAGE, rank=OverwatchRank.SILVER_4, total_games=12
-    )
-    player4 = Player(
-        user="player4", role=Role.TANK, rank=OverwatchRank.DIAMOND_5, total_games=12
-    )
-    player5 = Player(
-        user="player5", role=Role.SUPPORT, rank=OverwatchRank.MASTER_2, total_games=12
-    )
-    player6 = Player(
-        user="player6",
-        role=Role.DAMAGE,
-        rank=OverwatchRank.GRANDMASTER_5,
-        total_games=12,
+        user="player2", role=None, rank=ValorantRank.ASCENDANT_1, total_games=12
     )
     team1.add(player1)
-    team1.add(player2)
+    team2.add(player2)
+    assert mean_delta(team1, team2) == 0
+
+    player3 = Player(
+        user="player3",
+        role=None,
+        rank=ValorantRank.ASCENDANT_3,
+        total_games=12,
+    )
+    player4 = Player(
+        user="player4",
+        role=None,
+        rank=ValorantRank.ASCENDANT_3,
+        total_games=12,
+    )
     team1.add(player3)
     team2.add(player4)
-    team2.add(player5)
+    assert mean_delta(team1, team2) == 0
+    player5 = Player(
+        user="player5",
+        role=None,
+        rank=ValorantRank.DIAMOND_3,
+        total_games=12,
+    )
+    player6 = Player(
+        user="player6", role=None, rank=ValorantRank.BRONZE_3, total_games=12
+    )
+    team1.add(player5)
     team2.add(player6)
+    assert mean_delta(team1, team2) == 3.9999999999999982
+
+
+def test_overwatch_shuffle():
+    team1 = OverwatchTeam(name="team1")
+    team2 = OverwatchTeam(name="team2")
+
+    for i in range(1, 100):
+        player = Player(
+            user=f"player{i}",
+            role=Role(random.randint(1, 3)),
+            rank=OverwatchRank(random.randint(1, 36)),
+            total_games=12,
+        )
+        if i % 2 == 0:
+            team1.add(player)
+        else:
+            team2.add(player)
+
+    old_rank_delta = mean_delta(team1, team2)
+    shuffle(team1, team2)
+    new_rank_delta = mean_delta(team1, team2)
+    assert len(team1) == len(team2)
+    assert old_rank_delta != new_rank_delta # THIS IS RANDOM, SO IT CAN FAIL. 
+    # RE-RUN THE TEST IF IT FAILS
+
+
+def test_valorant_shuffle():
+    team1 = ValorantTeam(name="team1")
+    team2 = ValorantTeam(name="team2")
+
+    for i in range(1, 100):
+        player = Player(
+            user=f"player{i}",
+            role=None,
+            rank=ValorantRank(random.randint(1, 25)),
+            total_games=12,
+        )
+        if i % 2 == 0:
+            team1.add(player)
+        else:
+            team2.add(player)
+
     old_rank_delta = mean_delta(team1, team2)
     shuffle(team1, team2)
     new_rank_delta = mean_delta(team1, team2)
     assert len(team1) == len(team2)
     assert old_rank_delta != new_rank_delta
 
-    assert set(
-        [player1, player2, player3, player4, player5, player6]
-    ) == team1.players.union(team2.players)
+
+def test_overwatch_balance():
+    team1 = OverwatchTeam(name="team1")
+    team2 = OverwatchTeam(name="team2")
+
+    for i in range(1, 100):
+        player = Player(
+            user=f"player{i}",
+            role=Role(random.randint(1, 3)),
+            rank=OverwatchRank(random.randint(1, 36)),
+            total_games=12,
+        )
+        if i % 2 == 0:
+            team1.add(player)
+        else:
+            team2.add(player)
+
+    balance_teams(team1, team2)
+    assert len(team1) == len(team2)
+
+
+def test_valorant_balance():
+    team1 = ValorantTeam(name="team1")
+    team2 = ValorantTeam(name="team2")
+
+    for i in range(1, 100):
+        player = Player(
+            user=f"player{i}",
+            role=None,
+            rank=ValorantRank(random.randint(1, 25)),
+            total_games=12,
+        )
+        if i % 2 == 0:
+            team1.add(player)
+        else:
+            team2.add(player)
+
+    balance_teams(team1, team2)
+    assert len(team1) == len(team2)
+
+
+def test_player_add_overflow_void():
+    team1 = OverwatchTeam(name="team1")
+    team2 = OverwatchTeam(name="team2")
+
+    for i in range(1, 100):
+        player = Player(
+            user=f"player{i}",
+            role=Role(random.randint(1, 3)),
+            rank=OverwatchRank(random.randint(1, 36)),
+            total_games=12,
+        )
+        if i % 2 == 0:
+            team1.add(player)
+        else:
+            team2.add(player)
+
+    assert len(team1) == len(team2)
--- util/models/team.py
+++ util/models/team.py
@@ -1,18 +1,87 @@
+import copy
 import random
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
+from typing import Iterator, Self, Type, TypeVar
 
+from util.helpers.bounded_player_set import (
+    BoundedPlayerSet,
+    PlayerDoesntExist,
+    TeamIsFull,
+)
 from util.models.player import Player
-from util.helpers.bounded_player_set import BoundedPlayerSet, TeamIsFull, PlayerDoesntExist
 from util.models.role import Role
-from typing import Iterator
+
+T = TypeVar("T")
 
 
 @dataclass
-class Team:
+class Team(ABC):
+    @abstractmethod
+    def __contains__(self, player: Player) -> bool:
+        pass
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[Player]:
+        pass
+
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractmethod
+    def __getitem__(self, index: int) -> Player:
+        pass
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
+    @abstractmethod
+    def add(self, player: Player) -> bool:
+        """Adds a player to the team."""
+        pass
+
+    @abstractmethod
+    def remove(self, player: Player) -> bool:
+        """Removes a player from the team."""
+        pass
+
+    @abstractmethod
+    def clear(self):
+        """Removes all players from the team."""
+        pass
+
+    @abstractmethod
+    def rank_mean(self) -> float:
+        """Returns the mean rank of the team."""
+        pass
+
+    @abstractmethod
+    def is_full(self) -> bool:
+        """Returns True if the team is full, False otherwise."""
+        pass
+
+    @abstractmethod
+    def demo_clone(self: T) -> T:
+        """Returns a copy of the team with the same players."""
+        pass
+
+
+@dataclass(slots=True)
+class OverwatchTeam(Team):
     name: str
-    tank: BoundedPlayerSet[Player] = field(default_factory=lambda: BoundedPlayerSet(size=1, role=Role.TANK))
-    damage: BoundedPlayerSet[Player] = field(default_factory=lambda: BoundedPlayerSet(size=2, role=Role.DAMAGE))
-    support: BoundedPlayerSet[Player] = field(default_factory=lambda: BoundedPlayerSet(size=2, role=Role.SUPPORT))
+
+    tank: BoundedPlayerSet[Player] = field(
+        default_factory=lambda: BoundedPlayerSet(size=1)
+    )
+    damage: BoundedPlayerSet[Player] = field(
+        default_factory=lambda: BoundedPlayerSet(size=2)
+    )
+    support: BoundedPlayerSet[Player] = field(
+        default_factory=lambda: BoundedPlayerSet(size=2)
+    )
+    cloned: bool = field(default=False)
 
     def __contains__(self, player: Player) -> bool:
         return player in self.tank or player in self.damage or player in self.support
@@ -24,7 +93,9 @@ class Team:
         return len(self.tank | self.damage | self.support)
 
     def __getitem__(self, index) -> Player:
-        return list(self.tank | self.damage | self.support)[index]  # this is used for random.choice()
+        return list(self.tank | self.damage | self.support)[
+            index
+        ]  # this is used for random.choice()
 
     def __repr__(self) -> str:
         return f"Team({self.name}, {self.tank | self.damage | self.support})"
@@ -38,11 +109,10 @@ class Team:
                     self.damage.add(player)
                 case Role.SUPPORT:
                     self.support.add(player)
-            
+
             return True
         except TeamIsFull:
             return False
-
 
     def remove(self, player: Player) -> bool:
         try:
@@ -62,12 +132,78 @@ class Team:
         self.damage.clear()
         self.support.clear()
 
+    def rank_mean(self) -> float:
+        return sum([player.rank_value() for player in self]) / len(self)
+
+    def is_full(self) -> bool:
+        return len(self) == 5
+
+    def demo_clone(self: T) -> T:
+        team = type(self)(copy.copy(self.name))
+
+        team.cloned = True
+        team.tank = BoundedPlayerSet(size=1)
+        team.damage = BoundedPlayerSet(size=2)
+        team.support = BoundedPlayerSet(size=2)
+        for player in self:
+            team.add(player)
+        return team
+
+
+@dataclass(slots=True)
+class ValorantTeam(Team):
+    name: str
+    players: BoundedPlayerSet[Player] = field(
+        default_factory=lambda: BoundedPlayerSet(size=5)
+    )
+    cloned: bool = field(default=False)
+
+    def __contains__(self, player: Player) -> bool:
+        return player in self.players
+
+    def __iter__(self) -> Iterator[Player]:
+        return iter(self.players)
+
+    def __len__(self) -> int:
+        return len(self.players)
+
+    def __getitem__(self, index) -> Player:
+        return list(self.players)[index]
+
+    def __repr__(self) -> str:
+        return f"Team({self.name}, {self.players})"
+
+    def add(self, player: Player) -> bool:
+        try:
+            self.players.add(player)
+            return True
+        except TeamIsFull:
+            return False
+
+    def remove(self, player: Player) -> bool:
+        try:
+            self.players.remove(player)
+            return True
+        except PlayerDoesntExist:
+            return False
+
+    def clear(self):
+        self.players.clear()
 
     def rank_mean(self) -> float:
         return sum([player.rank_value() for player in self]) / len(self)
 
-    def is_full(self):
+    def is_full(self) -> bool:
         return len(self) == 5
+
+    def demo_clone(self: T) -> T:
+        team = type(self)(copy.copy(self.name))
+
+        team.cloned = True
+        team.players = BoundedPlayerSet(size=5)
+        for player in self:
+            team.add(player)
+        return team
 
 
 def mean_delta(team_1: Team, team_2: Team) -> float:
@@ -79,12 +215,45 @@ def shuffle(team1: Team, team2: Team):
 
     if (not team1.is_full()) and (not team2.is_full()):
         raise ValueError("Both teams must be full to shuffle.")
-    
+
     player1: Player = random.choice(team1)
     team1.remove(player1)
     for p in team2:
-        if p.role == player1.role:
+        if p.role == player1.role: # no special case needs to be made. Valorant roles should be set to NONE.
+            # in the future if we want to add more games, we can add a special case for each game.
             team2.remove(p)
             team2.add(player1)
             team1.add(p)
             return
+
+
+def balance_teams(team1: Team, team2: Team, iterations: int = 60):
+    if (not team1.is_full()) and (not team2.is_full()):
+        raise ValueError("Both teams must be full to shuffle.")
+
+    cloned_team1 = team1.demo_clone()
+    cloned_team2 = team2.demo_clone()
+
+    best_result: tuple[float, OverwatchTeam, OverwatchTeam] = (
+        float("inf"),
+        cloned_team1,
+        cloned_team2,
+    )  # store best results
+    # init values are (inf, team1 (cloned), team2 (cloned))
+
+    for _ in range(iterations):
+        shuffle(cloned_team1, cloned_team2)
+
+        if mean_delta(cloned_team1, cloned_team2) < best_result[0]:
+            best_result = (
+                mean_delta(cloned_team1, cloned_team2),
+                cloned_team1.demo_clone(),
+                cloned_team2.demo_clone(),
+            )
+
+    team1.clear()
+    team2.clear()
+    for player in best_result[1]:
+        team1.add(player)
+    for player in best_result[2]:
+        team2.add(player)
